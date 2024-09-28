ba32e344b7008a705ff374bf728f0cc4093a6062
Tokstech <138794535+Tokstech@users.noreply.github.com>
2024-09-28 12:21:59 +0100

    Update eCalculate.py
    I removed semi-colons from the code as it is not needed in current python versions. Also I made the code more interactive. The user will always be prompted the user to input a correct integer values for the code to run

--- eCalc/eCalculate.py
+++ eCalc/eCalculate.py
@@ -5,17 +5,19 @@ def CalculateE(roundVal):
 		someE = round(math.e,roundVal);
 		e = str(someE)
 		someList = list(e)
-		return someE;
+		return someE
 	
-	
-
-
-
-roundTo = raw_input('Enter the number of digits you want after the decimal for e: ')
-try:
-	roundint = int(roundTo);
-	print CalculateE(roundint);
-except:
-	print "You did not enter an integer";
+while True:
+    roundTo = input('Enter the number of digits you want after the decimal for e: ')
+    try:
+        roundint = int(roundTo)
+        print(CalculateE(roundint))
+        
+        break
+    
+    except:
+        print("You did not enter an integer")
+        
+        continue
 
 
